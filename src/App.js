0e23b2a621dbf7b606a80229382be4b925ada461
duyen <duyen.dinh25@student.passerellesnumeriques.org>
2024-06-07 21:36:47 +0700

    midterm 3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
-import Home from "./components/layout/Home";
 import Navbar from "./components/layout/Navbar";
-const App = () => {
+import Home from "./components/layout/Home";
+
+function App() {
+  const savedDarkMode = localStorage.getItem("isDarkMode") === "true";
+
+  const [isDarkMode, setIsDarkMode] = useState(savedDarkMode);
+  const [icon, setIcon] = useState(savedDarkMode ? faMoon : faSun);
+
+  useEffect(() => {
+    localStorage.setItem("isDarkMode", isDarkMode);
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode); 
+    setIcon(isDarkMode ? faSun : faMoon); 
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${isDarkMode ? "dark-theme" : ""}`}>
       <Router>
-        <Navbar />
-        <Home />
+        <div className="App">
+          <Navbar />
+          <div className="toggle-theme">
+            <FontAwesomeIcon
+              icon={icon}
+              onClick={toggleTheme}
+              style={{ color: "yellow" }}
+            />
+          </div>
+          <Home />
+        </div>
       </Router>
     </div>
   );
-};
+}
+
 export default App;
